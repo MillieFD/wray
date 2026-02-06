b83396b44dc66e427c59708aa52ddfd226691d68
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-02-06 22:15:34 +0000

    Adjusted file spacing

--- src/database/measurements/accumulator.rs
+++ src/database/measurements/accumulator.rs
@@ -7,6 +7,7 @@ BSD 3-Clause License, Copyright (c) 2026, Amelia Fraser-Dale
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the conditions of the LICENSE are met.
 */
+
 use std::sync::Arc;
 use std::time::SystemTime;
 

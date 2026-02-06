8e4e22e74361da504cd9f4b7915ae0390468b6c7
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-02-06 20:10:57 +0000

    Add `writer` module and update `Database` structure to use `Writer` implementations

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -8,57 +8,58 @@ Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the conditions of the LICENSE are met.
 */
 
+/* ----------------------------------------------------------------------------- Private Modules */
+
 mod measurements;
 mod spectra;
 mod spectrometers;
 mod table;
+mod writer;
+
+/* ----------------------------------------------------------------------------- Private Imports */
 
 use std::fs;
 use std::path::PathBuf;
 
 use measurements::*;
+use spectra::*;
 use spectrometers::*;
-use table::*;
+use writer::*;
 
-struct Database {
+use crate::Error;
+
+/* ------------------------------------------------------------------------------ Public Exports */
+
+pub struct Database {
     path: PathBuf,
-    spectrometers: Spectrometers,
-    measurements: Measurements,
+    wavelengths: WavelengthWriter,
+    measurements: MeasurementWriter,
+    intensities: IntensityWriter,
 }
 
 impl Database {
-    pub fn new(filename: &str) -> Result<Database, std::io::Error> {
-        fs::create_dir_all(filename)?;
-        let path = std::env::current_dir()?.join(filename).canonicalize()?;
-        let spectrometers = Spectrometers::new(&path)?;
-        let measurements = Measurements::new(&path)?;
+    pub fn new(filepath: &str) -> Result<Database, Error> {
+        fs::DirBuilder::new().recursive(true).create(&filepath)?;
+        let buf = PathBuf::from(filepath).canonicalize()?;
         let db = Database {
-            path,
-            spectrometers,
-            measurements,
+            wavelengths: buf.as_path().try_into()?,
+            measurements: buf.as_path().try_into()?,
+            intensities: buf.as_path().try_into()?,
+            path: buf,
         };
         Ok(db)
     }
 }
 
+/* ---------------------------------------------------------------------------------- Unit Tests */
+
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
-
-    use polars::prelude::*;
-
     use super::*;
 
     #[test]
     fn database_creation() {
         let db = Database::new("test").unwrap();
         assert!(db.path.exists());
-    }
-
-    fn inspect_ipc_schema(path: &Path) {
-        let file = fs::File::open(path).unwrap();
-        let mut reader = IpcStreamReader::new(file);
-        let schema = reader.schema().unwrap();
-        println!("{:?}", schema);
     }
 }

d5587f36238eac82b5c2d6a03e5d7aaf97a81807
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-02-06 19:58:06 +0000

    Add Accumulator and MeasurementWriter for managing measurement data in IPC format

---
+++ src/database/measurements/accumulator.rs
@@ -0,0 +1,81 @@
+/*
+Project: Optic
+GitHub: https://github.com/MillieFD/optic
+
+BSD 3-Clause License, Copyright (c) 2026, Amelia Fraser-Dale
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the conditions of the LICENSE are met.
+*/
+use std::sync::Arc;
+use std::time::SystemTime;
+
+use arrow::array::{Array, Int32Builder, UInt8Builder, UInt32Builder, UInt64Builder};
+use uom::si::i32::Length;
+use uom::si::length::micrometer;
+use uom::si::time::microsecond;
+use uom::si::u32::Time;
+
+/* ------------------------------------------------------------------------------ Public Exports */
+
+pub(super) struct Accumulator {
+    rows: u32,
+    id: UInt32Builder,
+    timestamp: UInt64Builder,
+    x: Int32Builder,
+    y: Int32Builder,
+    z: Int32Builder,
+    a: Int32Builder,
+    // r: Int32Builder,
+    duration: UInt32Builder,
+    spectrometer: UInt8Builder,
+}
+
+impl Accumulator {
+    pub(super) fn new() -> Self {
+        Self {
+            rows: 0,
+            id: Default::default(),
+            timestamp: Default::default(),
+            x: Default::default(),
+            y: Default::default(),
+            z: Default::default(),
+            a: Default::default(),
+            // r: Default::default(),
+            duration: Default::default(),
+            spectrometer: Default::default(),
+        }
+    }
+
+    pub fn push(&mut self, x: Length, y: Length, z: Length, a: Length, i: Time, spectrometer: u8) {
+        let timestamp = SystemTime::UNIX_EPOCH
+            .elapsed()
+            .unwrap_or_default()
+            .as_millis() as u64;
+        self.id.append_value(self.rows);
+        self.timestamp.append_value(timestamp);
+        self.x.append_value(x.get::<micrometer>());
+        self.y.append_value(y.get::<micrometer>());
+        self.z.append_value(z.get::<micrometer>());
+        self.a.append_value(a.get::<micrometer>());
+        // self.r.append_value(r.get::<radian>());
+        self.duration.append_value(i.get::<microsecond>());
+        self.spectrometer.append_value(spectrometer);
+        self.rows += 1;
+    }
+
+    pub(super) fn columns(&mut self) -> Vec<Arc<dyn Array>> {
+        vec![
+            Arc::new(UInt32Builder::finish(&mut self.id)),
+            Arc::new(UInt64Builder::finish(&mut self.timestamp)),
+            Arc::new(Int32Builder::finish(&mut self.x)),
+            Arc::new(Int32Builder::finish(&mut self.y)),
+            Arc::new(Int32Builder::finish(&mut self.z)),
+            Arc::new(Int32Builder::finish(&mut self.a)),
+            Arc::new(UInt32Builder::finish(&mut self.duration)),
+            Arc::new(UInt8Builder::finish(&mut self.spectrometer)),
+        ]
+    }
+}
+
+/* ----------------------------------------------------------------------- Trait Implementations */
---
+++ src/database/measurements/mod.rs
@@ -0,0 +1,96 @@
+/*
+Project: Optic
+GitHub: https://github.com/MillieFD/optic
+
+BSD 3-Clause License, Copyright (c) 2026, Amelia Fraser-Dale
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the conditions of the LICENSE are met.
+*/
+
+/* ----------------------------------------------------------------------------- Private Modules */
+
+mod accumulator;
+
+/* ----------------------------------------------------------------------------- Private Exports */
+
+use std::fs::File;
+use std::path::Path;
+use std::sync::{Arc, LazyLock};
+
+use accumulator::*;
+use arrow::datatypes::DataType::{Int32, Timestamp, UInt32};
+use arrow::datatypes::TimeUnit::Microsecond;
+use arrow::datatypes::{Field, Schema};
+use arrow::error::ArrowError;
+use arrow::ipc::writer::StreamWriter;
+use arrow::record_batch::RecordBatch;
+
+use super::traits::Writer;
+use crate::Error;
+
+/* ------------------------------------------------------------------------------ Public Exports */
+
+pub(super) struct MeasurementWriter {
+    writer: StreamWriter<File>,
+    acc: Accumulator,
+}
+
+impl MeasurementWriter {
+    pub(super) fn new<P>(path: P) -> Result<Self, Error>
+    where
+        P: AsRef<Path>,
+    {
+        let path = path.as_ref().join("measurements.ipc");
+        let writer = File::create(path)?.try_into()?;
+        Ok(writer)
+    }
+
+    pub fn commit(&mut self) {
+        let columns = self.acc.columns();
+        let batch = RecordBatch::try_new(Self::schema(), columns).unwrap();
+        self.writer.write(&batch).expect("Failed to write batch");
+    }
+}
+
+/* ----------------------------------------------------------------------- Trait Implementations */
+
+impl TryFrom<File> for MeasurementWriter {
+    type Error = ArrowError;
+
+    fn try_from(file: File) -> Result<Self, Self::Error> {
+        let writer = StreamWriter::try_new(file, &Self::SCHEMA)?.into();
+        Ok(writer)
+    }
+}
+
+impl From<StreamWriter<File>> for MeasurementWriter {
+    fn from(writer: StreamWriter<File>) -> Self {
+        let acc = Accumulator::new();
+        Self { writer, acc }
+    }
+}
+
+impl TryFrom<&Path> for MeasurementWriter {
+    type Error = Error;
+
+    fn try_from(path: &Path) -> Result<Self, Self::Error> {
+        Self::new(path)
+    }
+}
+
+impl Writer for MeasurementWriter {
+    const SCHEMA: LazyLock<Arc<Schema>> = LazyLock::new(|| {
+        let fields = [
+            Field::new("measurement_id", UInt32, false).into(),
+            Field::new("timestamp", Timestamp(Microsecond, None), false).into(),
+            Field::new("x_coordinate", Int32, false).into(),
+            Field::new("y_coordinate", Int32, false).into(),
+            Field::new("z_coordinate", Int32, false).into(),
+            Field::new("a_interfibre", Int32, false).into(),
+            Field::new("integration_duration", UInt32, false).into(),
+            Field::new("spectrometer_id", UInt32, false).into(),
+        ];
+        Schema::new(fields).into()
+    });
+}

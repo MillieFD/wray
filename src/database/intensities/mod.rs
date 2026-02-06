f1b773d6d38ed223fe2329121934b4c7d2be0ee9
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-02-06 22:12:26 +0000

    Created new `intensities` module as a directory

---
+++ src/database/intensities/mod.rs
@@ -0,0 +1,9 @@
+/*
+Project: Optic
+GitHub: https://github.com/MillieFD/optic
+
+BSD 3-Clause License, Copyright (c) 2026, Amelia Fraser-Dale
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the conditions of the LICENSE are met.
+*/

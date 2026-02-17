ef61232790bad7e6fa117fb3cafe214173c771ef
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-02-17 22:55:17 +0000

    Move `Measurements` module to top crate level with `push`, `commit`, and IPC streaming support.

---
+++ src/measurements/mod.rs
@@ -0,0 +1,96 @@
+/*
+Project: Optic
+GitHub: https://github.com/MillieFD/optic
+
+BSD 3-Clause License, Copyright (c) 2026, Amelia Fraser-Dale
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the conditions of the LICENSE are met.
+*/
+
+/* ----------------------------------------------------------------------------- Private Modules */
+
+mod builder;
+
+/* ----------------------------------------------------------------------------- Private Imports */
+
+use std::fs::{File, OpenOptions};
+use std::path::{Path, PathBuf};
+use std::sync::{Arc, LazyLock};
+
+use arrow::array::{AsArray, RecordBatch};
+use arrow::datatypes::DataType::{Int32, Timestamp, UInt32};
+use arrow::datatypes::TimeUnit::Microsecond;
+use arrow::datatypes::{Field, Schema};
+use arrow::ipc::writer::StreamWriter;
+use uom::si::f64::{Length, Time};
+
+use self::builder::*;
+use crate::{Error, Writer};
+
+/* ------------------------------------------------------------------------------ Public Exports */
+
+pub struct Measurements {
+    stream: StreamWriter<File>,
+    builder: Builder,
+}
+
+impl Measurements {
+    pub(super) fn new<P>(path: P) -> Result<Self, Error>
+    where
+        P: AsRef<Path>,
+    {
+        path.as_ref()
+            .join("measurements")
+            .with_extension("arrow")
+            .try_into()
+    }
+
+    pub fn push(&mut self, x: Length, y: Length, z: Length, a: Length, i: Time) -> u32 {
+        self.builder.push(x, y, z, a, i)
+    }
+
+    pub fn commit(&mut self) -> Result<(), Error> {
+        let columns = self.builder.columns();
+        let batch = RecordBatch::try_new(*Self::SCHEMA, columns)?;
+        self.stream.write(&batch).map_err(Error::from)
+    }
+}
+
+/* ----------------------------------------------------------------------- Trait Implementations */
+
+impl Writer for Measurements {
+    const SCHEMA: LazyLock<Arc<Schema>> = LazyLock::new(|| {
+        let fields = [
+            Field::new("id", UInt32, false).into(),
+            Field::new("timestamp", Timestamp(Microsecond, None), false).into(),
+            #[cfg(feature = "x")]
+            Field::new("x", Int32, false).into(),
+            #[cfg(feature = "y")]
+            Field::new("y", Int32, false).into(),
+            #[cfg(feature = "z")]
+            Field::new("z", Int32, false).into(),
+            #[cfg(feature = "a")]
+            Field::new("a", Int32, false).into(),
+            Field::new("integration", UInt32, false).into(),
+        ];
+        Schema::new(fields).into()
+    });
+}
+
+impl TryFrom<PathBuf> for Measurements {
+    type Error = Error;
+
+    fn try_from(path: PathBuf) -> Result<Self, Self::Error> {
+        let stream: StreamWriter<File> = OpenOptions::new()
+            .read(false)
+            .write(true)
+            .create(true)
+            .truncate(false)
+            .open(&path)?
+            .into();
+        let builder = Builder::new(&path);
+        let table = Self { stream, builder };
+        Ok(table)
+    }
+}
